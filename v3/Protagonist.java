c4a02565e50b5a1e4a9e8f0bc69fd49e1b9b76b3
Maggie <mzhao3@stuy.edu>
2017-11-13 22:05:14 -0500

    Robustified Protagonist code.

--- v3/Protagonist.java
+++ v3/Protagonist.java
@@ -4,52 +4,42 @@
 //2017-11-10
 
 public class Protagonist extends Character{
-	private static String name;
-	public static int hitpoints;
-	private static int strength;
-	public static int defense;
-	private static double attack;
+    private String name;
 	
-	public Protagonist(String input){
-		name = input;
-		hitpoints = 400;
-		strength = 45;
-		defense = 25;
-		attack = 2;
+    public Protagonist(String input){
+	name = input;
+	HP = 400;
+	strength = 45;
+	defense = 25;
+	attack = 2;
+    }
+	
+    public String getName(){
+	return name;
+    }
+    
+    // Attack Method
+    public int attack(Monster x){
+        int pain = (int)((strength * attack) - x.getDefense());
+
+	if (pain < 0) {
+	    pain = 0;
 	}
 	
-	//returns true if hp > 0, false otherwise
-	public boolean isAlive(){
-		return hitpoints > 0;
-	}
-	//defense accessor
-	public int getDefense(){
-		return defense;
-	}
-	//name accessor
-	public String getName(){
-		return name;
-	}
-	//lowers hitpoints instance variable
-	//paramter: integer damage
-	public void lowerHP(int damage){
-		hitpoints -= damage;
-	}
-	//calculates damage dealt
-	//parameter: monster object
-	public int attack(Monster x){
-		int pain = (int)(strength * attack) - x.defense;
-		x.lowerHP(pain);
-		return pain;
-	}
-	//changes stats
-	public void specialize(){
-		defense /= 2;
-		attack *= 2;
-	}
-	//resets stats
-	public void normalize(){
-		defense = 25;
-		attack = 2;
-	}
+        x.lowerHP(pain);
+        return pain;
+    }
+    
+    //changes stats
+    public void specialize(){
+	defense /= 2;
+	attack *= 2;
+    }
+    
+    //resets stats
+    public void normalize(){
+	defense = 25;
+	attack = 2;
+    }
+    
 }
