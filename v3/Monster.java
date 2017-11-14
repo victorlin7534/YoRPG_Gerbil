2d262ae37f9c062bdcad77c4a7c7c3d517df5f5b
Maggie <mzhao3@stuy.edu>
2017-11-13 22:07:48 -0500

    Robustified Monster code.

--- v3/Monster.java
+++ v3/Monster.java
@@ -4,42 +4,26 @@
 //2017-11-10
 
 public class Monster extends Character{
-	private static String name;
-	public static int hitpoints;
-	private static int strength;
-	public static int defense;
-	private static double attack;
 	
-	public Monster(){
-		name = "monster";
-		strength = (int)(Math.random() * (45)) + 20;
-		hitpoints = 150;
-		defense = 20;
-		attack = 1.0;
+    public Monster(){
+	strength = (int)(Math.random() * (45)) + 20;
+	HP = 150;
+	defense = 20;
+	attack = 1.0;
+    }
+
+
+    //calculates damage dealt
+    //parameter: protagonist object
+    public int attack(Protagonist x){
+	int pain = (int)((strength * attack) - x.defense);
+
+	if (pain < 0){
+	    pain = 0;
 	}
 	
-	//returns true if hp > 0, false otherwise
-	public boolean isAlive(){
-		return hitpoints > 0;
-	}
-	//defense accessor
-	public int getDefense(){
-		return defense;
-	}
-	//name accessor
-	public String getName(){
-		return name;
-	}
-	//lowers hitpoints instance variable
-	//paramter: integer damage
-	public void lowerHP(int damage){
-		hitpoints -= damage;
-	}
-	//calculates damage dealt
-	//parameter: protagonist object
-	public int attack(Protagonist x){
-		int pain = (int)(strength * attack) - x.defense;
-		x.lowerHP(pain);
-		return pain;
-	}
+	x.lowerHP(pain);
+	return pain;
+    }
+
 }
